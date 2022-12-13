fee409ea66171bad7a648a39f13fecf8811d7550
Vlad Frangu <kingdgrizzle@gmail.com>
2022-12-13 12:04:14 +0200

    chore: cmon

---
+++ scripts/actions/markPrivate.mjs
@@ -0,0 +1,22 @@
+import { readdir, readFile, writeFile } from 'node:fs/promises';
+
+const [_, packageToIgnore] = process.argv;
+
+if (!packageToIgnore) {
+	throw new Error('Missing package provided to ignore from marking as private');
+}
+
+const packagesDir = new URL('../packages/', import.meta.url);
+
+const packages = await readdir(packagesDir);
+
+for (const pkg of packages) {
+	if (pkg === packageToIgnore) {
+		continue;
+	}
+
+	const pkgJsonPath = new URL(`${pkg}/package.json`, packagesDir);
+	const pkgJson = JSON.parse(await readFile(pkgJsonPath, 'utf-8'));
+	pkgJson.private = true;
+	await writeFile(pkgJsonPath, JSON.stringify(pkgJson, null, '\t'));
+}
---
+++ scripts/actions/unmarkPrivate.mjs
@@ -0,0 +1,14 @@
+import { readdir, readFile, writeFile } from 'node:fs/promises';
+
+const packagesDir = new URL('../packages/', import.meta.url);
+
+const packages = await readdir(packagesDir);
+
+for (const pkg of packages) {
+	const pkgJsonPath = new URL(`${pkg}/package.json`, packagesDir);
+	const pkgJson = JSON.parse(await readFile(pkgJsonPath, 'utf-8'));
+
+	delete pkgJson.private;
+
+	await writeFile(pkgJsonPath, JSON.stringify(pkgJson, null, '\t'));
+}
